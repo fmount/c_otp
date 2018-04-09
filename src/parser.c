5fcac635283302848273fd0bab72dc4b642a02e0
fmount <fmount@autistici.org>
2018-04-09 23:13:44 +0200

    [+] Prototyping the providerrc parser

--- src/parser.c
+++ src/parser.c
@@ -3,94 +3,116 @@
 #include<stdlib.h>
 #include<errno.h>
 #include<string.h>
+#include<unistd.h>
 
 
 typedef struct {
-	char *pname;
-	char *psecret;
+    char *pname;
+    char *psecret;
 } PROVIDER;
 
 
 PROVIDER *split_str(char *spl, char delim)
 {
-	char *tmp_name;
-	char *tmp_secret;
-	PROVIDER *p;
-	size_t count = 0;
+    char *tmp_name;
+    char *tmp_secret;
+    PROVIDER *p;
+    size_t count = 0;
 
-	//Get break point
-	do {
-		count++;
+    //Get break point
+    do {
+        count++;
+    } while (spl[count] != delim);
 
-	} while (spl[count] != delim);
 
+    tmp_name = (char *) malloc(count * sizeof(char));
+    tmp_secret = (char *) malloc((strlen(spl)-count) * sizeof(char));
 
-	tmp_name = (char *) malloc(count * sizeof(char));
-	tmp_secret = (char *) malloc((strlen(spl)-count) * sizeof(char));
+    //Get first part of the string
+    //for (int i = 0; i < count; i++)
+    //  tmp_name[i] = spl[i];
 
-	//Get first part of the string
-	//for (int i = 0; i < count; i++)
-	//	tmp_name[i] = spl[i];
+    //Get first part of the string
+    memcpy(tmp_name, spl, count);
 
-	//Get first part of the string
-	memcpy(tmp_name, spl, count);
-	
-	//Get second part of the string
-	memcpy(tmp_secret, spl+strlen(tmp_name), strlen(spl)-strlen(tmp_name));
+    //Get second part of the string
+    memcpy(tmp_secret, spl+strlen(tmp_name), strlen(spl)-strlen(tmp_name));
 
-	//Get second part of the string
-	//for (int i = (count+1), j = 0; i < strlen(spl); i++, j++)
-	//	tmp_secret[j] = spl[i];
+    //Get second part of the string
+    //for (int i = (count+1), j = 0; i < strlen(spl); i++, j++)
+    //  tmp_secret[j] = spl[i];
 
 #if DEBUG
 
-	printf("[GOT LEN]: %d\n", strlen(spl));
-	printf("[PROVIDER SECTION]: %d characters\n", count);
-	printf("[GOT NAME]: %s\n", tmp_name);
-	printf("[SECRET SECTION]: %d\n", (strlen(spl)-count+1));
-	printf("[GOT SECRET]: %s\n", tmp_secret);
+    printf("[GOT LEN]: %d\n", strlen(spl));
+    printf("[PROVIDER SECTION]: %d characters\n", count);
+    printf("[GOT NAME]: %s\n", tmp_name);
+    printf("[SECRET SECTION]: %d\n", (strlen(spl)-count+1));
+    printf("[GOT SECRET]: %s\n", tmp_secret);
 
 #endif
 
-	p = malloc(sizeof(PROVIDER));
-	p->pname = tmp_name;
-	p->psecret = tmp_secret;
+    p = malloc(sizeof(PROVIDER));
+    p->pname = tmp_name;
+    p->psecret = tmp_secret;
 
-	return p;
+    return p;
 
 }
 
 void process_provider(char *line, size_t len)
 {
-	PROVIDER *p;
+    PROVIDER *p;
 
-	p = split_str(line, ':');
+    p = split_str(line, ':');
 
-	printf("GOT PROVIDER %s with secret %s\n", p->pname, p->psecret);
+    printf("GOT PROVIDER %s with secret %s\n", p->pname, p->psecret);
 
 }
 
 int main(int argc, char **argv)
 {
 
-	FILE *f;
-	size_t len = 1024;
-	char *line = NULL;
+    FILE *f;
+    size_t len = 1024;
+    char *fname = NULL;
+    int opt;
 
-	f = fopen("providersrc", "r");
+    if(argc <= 1) {
+        fprintf(stderr, "Provide at least one argument\n");
+        return -1;
+    }
 
-	if (f == NULL)
-		exit(ENOENT);
+    while((opt = getopt(argc, argv, "f:v")) != -1 ) {
+        switch(opt) {
+            case 'f':
+              //fname = argv[0];
+              fname = optarg;
+              break;
+            case 'v':
+              break;
+            default:
+              fprintf(stderr, "Usage: %s [-f fname]\n", argv[0]);
+        }
+    }
 
-	while (getline(&line, &len, f) != -1)
-		if (line[0] == '#')
-			printf("Get a comment, ignore it\n");
-		else
-			process_provider(line, len);
+    printf("Got fname %s\n", fname);
 
-	free(line);
-	exit(EXIT_SUCCESS);
+    f = fopen(fname, "r");
 
+    if (f == NULL)
+        exit(ENOENT);
 
-	return 0;
+    char *line = NULL;
+
+    while (getline(&line, &len, f) != -1)
+        if (line[0] == '#')
+            printf("Get a comment, ignore it\n");
+        else
+            process_provider(line, len);
+    free(line);
+
+    exit(EXIT_SUCCESS);
+
+    return 0;
 }
