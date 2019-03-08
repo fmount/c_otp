29c3ee95cde48032ab7792d50c8e690cc8205f0b
fmount <francesco.pan001@gmail.com>
2019-03-08 18:09:00 +0100

    [+] Redesign cli user interaction including gpg feature
        Add some util functions to verify providerrc file
        exists, including a sample.gpg for test purposes.
        Cli user interaction redesign to take care of the
        gpg mode and user parameters to be passed to the
        main

--- lib/utils.h
+++ lib/utils.h
@@ -19,12 +19,13 @@
 #include<stdint.h>
 #include<stdlib.h>
 #include<string.h>
-
+#include<sys/stat.h>
 #include "plist.h"
 
 PROVIDER *split_str(char *spl, char delim);
 PROVIDER *process_provider(NODE **plist, char *line);
 int validate_b32key(char *k, size_t len, size_t pos);
+int file_exists(const char *fpath);
 size_t decode_b32key(uint8_t **k, size_t len);
 
 #endif
